29e91f4281d932368be51afc562231d5a0430bf2
Lucas Hardman <lucas.hardman92@gmail.com>
2021-05-14 15:51:32 -0300

    model courses

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-# require "active_storage/engine"
+# require "active_storage/engine" <----- usar este pra subir a fotooo!!!!!
 require "action_controller/railtie"
 require "action_mailer/railtie"
 # require "action_mailbox/engine"
