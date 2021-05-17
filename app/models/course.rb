b1124491f2f28548b2c2177b0baa3fe925d9de66
Lucas Hardman <lucas.hardman92@gmail.com>
2021-05-17 17:17:02 -0300

    validações no cadastro de cursos

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+    validates :name, :price, :code, presence: {message: 'não pode ficar em branco'} #teste unitário. testa o model.
+    validates :code, uniqueness: {message: 'já está em uso'}
 end
