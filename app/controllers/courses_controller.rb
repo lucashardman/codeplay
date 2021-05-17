e97505d1a05c5401f5b86ddbd2580a8e435ceb1b
Lucas Hardman <lucas.hardman92@gmail.com>
2021-05-17 16:43:27 -0300

    cadastrar curso

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,7 +3,23 @@ class CoursesController < ApplicationController
         @courses = Course.all
     end
 
+    def new
+        @course = Course.new
+    end
+
+    def create
+        @course = Course.new(course_params)
+        @course.save
+        redirect_to @course
+    end
+
     def show
         @course = Course.find(params[:id])
+    end
+
+    private
+
+    def course_params
+        params.require(:course).permit(:name, :description, :code, :price, :enrollment_deadline)
     end
 end
